ede9bfb0061cafde442ef56d71289562ff97201d
Jiratip.W <jiratip.w@gmail.com>
2019-02-27 06:00:25 +0700

    add calculate api

--- admin/calculate.js
+++ admin/calculate.js
@@ -5,10 +5,42 @@ const db = require('../db');
 module.exports = router;
 
 router.get('/list', (req, res, next) => {
-    const sql = `select * from calculate`;
+    const sql = `select *
+                 from calculate`;
     db.query(sql)
         .then(([result]) => {
             res.json(result);
+        })
+        .catch(error => {
+            res.status(500)
+                .json(null);
+        });
+});
+
+router.post('/', (req, res, next) => {
+    const body = {...req.body};
+    const sql = `insert into calculate(name, nameEN)
+                 values (?, ?)`;
+    db.query(sql, [body.name, body.nameEN])
+        .then(([result]) => {
+            res.json(result);
+        })
+        .catch(error => {
+            res.status(500)
+                .json(null);
+        });
+});
+
+router.put('/{id}', (req, res, next) => {
+    const body = {...req.body};
+    const param = req.params;
+    const sql = `update calculate
+                 set name=?,
+                     nameEN=?
+                 where id = ?`;
+    db.query(sql, [body.name, body.nameEN, Number(body.id)])
+        .then(([result]) => {
+            res.json({status: 'success'});
         })
         .catch(error => {
             res.status(500)
---
+++ sql/2019-02-27-add-column-in-calculate.sql
@@ -0,0 +1,2 @@
+alter table calculate
+	add nameEN varchar(200) null after name;
